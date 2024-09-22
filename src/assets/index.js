c3fdbc5b08c887dfe0acc340747ce34a2ba302fd
OsamaM0 <osamamohamedmohamed30@gmail.com>
2024-09-23 02:17:21 +0300

    Version 1

---
+++ src/assets/index.js
@@ -0,0 +1,75 @@
+import airbnb from "./airbnb.png";
+import bill from "./bill.png";
+import binance from "./binance.png";
+import card from "./card.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.png";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import people04 from "./people04.png";
+import mobile_dev from "./mobile_dev.png";
+import web_dev from "./web_dev.png";
+import bot_dev from "./bot_dev.png";
+import analysis_dev from "./analysis_dev.png";
+import desktop_dev from "./desktop_dev.png";
+import ai_dev from "./ai_dev.png";
+import app_akhbary from "./app_akhbary.jpg";
+import web_bookify from "./web_bookify.jpg";
+import app_ozbek from "./app_ozbek.jpg";
+import app_mezo from "./app_mezo.png"
+
+export {
+  airbnb,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  logo,
+  quotes,
+  robot,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+  people04,
+  mobile_dev,
+  bot_dev,
+  web_dev,
+  ai_dev,
+  analysis_dev,
+  desktop_dev,
+  app_akhbary,
+  app_ozbek,
+  web_bookify,
+  app_mezo
+};
