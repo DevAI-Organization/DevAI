0ce0cc09db45b3cdbb9afd437ba63ea036f61ac3
Osama Mohamed <123073977+OsamaM0@users.noreply.github.com>
2024-09-23 11:29:54 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/DevAI/', // This is the subdirectory where your app will be deployed
   plugins: [react()],
-})
+});
